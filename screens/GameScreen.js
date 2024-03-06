463b45b02137fe187ecb025e68420609e6b7da62
vianrove <vianrove@gmail.com>
2024-03-05 19:32:25 -0500

    Created GameScreen

---
+++ screens/GameScreen.js
@@ -0,0 +1,22 @@
+import { View, StyleSheet } from "react-native";
+import Title from "../components/Title";
+
+function GameScreen() {
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's guess </Title>
+
+      <View></View>
+      <View></View>
+    </View>
+  );
+}
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 34,
+  },
+});
